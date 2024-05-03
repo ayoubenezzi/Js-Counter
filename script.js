464c187b730b72977f45eac2feae63cbb5172d04
Ayoub Benezzi <142675900+ayoubenezzi@users.noreply.github.com>
2024-05-03 19:30:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let number = 0;
+function increment() {
+  const number = Number(document.getElementById("number").innerHTML);
+  document.getElementById("number").innerHTML = number + 1;
+}
+
+function decrement() {
+  const number = Number(document.getElementById("number").innerHTML);
+  document.getElementById("number").innerHTML = number - 1;
+}
+
+function neutral() {
+  document.getElementById("number").innerHTML = number;
+}
